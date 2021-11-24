b1f2568d17c5bf926ab993446546e9da3d54d196
Dang Minh Nguyen <dangnm.cs@gmail.com>
2021-11-25 02:10:27 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Heading>Shopify app with Node and React</Heading>
 
     <div>This is Dashboard App!</div>
   </Page>
