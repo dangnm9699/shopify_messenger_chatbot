e1affc18fc83e94f11bba989284de81c0ce7dabf
tuanbmhust <tuanbm.hust@gmail.com>
2021-12-27 00:06:45 +0700

    Add configs

--- messenger_chatlog_dashboard/config/config.js
+++ messenger_chatlog_dashboard/config/config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 require('dotenv').config()
 
-exports.userMongoDB = process.env.MONGO_USER || "tuantungdang"
-exports.passwordMongoDB = process.env.MONGO_PASSWORD || "dangtuantung"
-exports.MongoDB = process.env.MONGO_DB || "rasa"
-exports.CollectionMongoDB = process.env.MONGO_COLLECTION || "conversations"
-exports.ClusterMongoDB = process.env.MONGO_CLUSTER || "cluster0.ibd9d.mongodb.net"
+exports.userMongoDB = process.env.MONGO_USER 
+exports.passwordMongoDB = process.env.MONGO_PASSWORD 
+exports.MongoDB = process.env.MONGO_DB 
+exports.CollectionMongoDB = process.env.MONGO_COLLECTION 
+exports.ClusterMongoDB = process.env.MONGO_CLUSTER 
